597e8c87ac3251d19cae8460ae3291532bf6fad2
Tommy Han <tommy_han1@intuit.com>
2021-12-30 11:12:59 -0500

    try catch test

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello World');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass that to video element, then play
+async function selectMediaStream() {
+    try {
+
+        something
+
+    } catch(error) {
+        // Catch Error Here
+        console.log('whoops, error here:', error) 
+    }
+}
+
+// OnLoad
+selectMediaStream();
